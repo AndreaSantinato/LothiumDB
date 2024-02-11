7b03af24e632108dc8e3060e2d4e2093c97b1fa0
Andrea Santinato <santinato.andrea.santi@gmail.com>
2024-02-11 16:05:31 +0100

    Added internal exceptions
    Added specific internal exception to handle internal library errors.

---
+++ LothiumDB/Exceptions/DatabaseException.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LothiumDB.Exceptions;
+
+/// <summary>
+/// Define a DatabaseConfiguration custom exception
+/// It provided a set of methods to validate the db's configuration object
+/// </summary>
+internal class DatabaseException : Exception
+{
+    #region Constructors
+
+    public DatabaseException() : base() { }
+
+    public DatabaseException(string? message) : base(message) { }
+
+    public DatabaseException(string? message, DatabaseException? innerException) : base(message, innerException) { }
+
+    #endregion Constructors
+
+    /// <summary>
+    /// Throw an exception if the passed connection is not valid to perform db's operations
+    /// </summary>
+    /// <param name="connection">Contains the database context's connection</param>
+    /// <exception cref="ArgumentException"></exception>
+    internal static void ThrowIfConnectionIsNull([NotNull] IDbConnection? connection)
+    {
+        if (connection is null)
+            ThrowException("The connection must be declared and created to perform database operations!");
+
+        if (string.IsNullOrEmpty(connection.ConnectionString))
+            ThrowException("The connection string must be specified to perform database operations!");
+    }
+
+    [DoesNotReturn]
+    private static void ThrowException(string? errorMessage)
+    => throw new DatabaseException(errorMessage);
+}
---
+++ LothiumDB/Exceptions/SqlBuilderException.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using LothiumDB.Tools;
+
+namespace LothiumDB.Exceptions;
+
+/// <summary>
+/// Define a SqlBuilder Exception
+/// </summary>
+internal class SqlBuilderException : Exception
+{
+    #region Constructors
+
+    public SqlBuilderException() : base() { }
+
+    public SqlBuilderException(string? message) : base(message) { }
+
+    public SqlBuilderException(string? message, SqlBuilderException? innerException) : base(message, innerException) { }
+
+    #endregion Constructors
+
+    /// <summary>
+    /// Throw an exception if the passed sql builder is not valid to perform
+    /// operations into the database's instance
+    /// </summary>
+    /// <param name="sqlBuilder">Contains the sql builder to verify</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static void ThrowIfBuilderIsNullorEmpty([NotNull] SqlBuilder? sqlBuilder)
+    {
+        if (sqlBuilder is null)
+            ThrowException("The provided builder is not valid!");
+
+        ThrowIfSqlNullOrEmpty(sqlBuilder.Query, sqlBuilder.Params);
+    }
+
+    /// <summary>
+    /// Throw an exception if the passed sql and arguments are not valid to perform
+    /// operations into the database's instance
+    /// </summary>
+    /// <param name="sql">Contains the query to verify</param>
+    /// <param name="args">Contains the query's arguments to verify</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static void ThrowIfSqlNullOrEmpty([NotNull] string? sql, object[]? args)
+    {
+        if (string.IsNullOrEmpty(sql)) 
+            ThrowException("The provided sql query is not valid!");
+
+        if (sql.Contains('@') && (args is null || !args.Any())) 
+            ThrowException("The providee sql query have declared variables but no passed parameters!");
+    }
+
+    [DoesNotReturn]
+    private static void ThrowException(string? errorMessage)
+        => throw new SqlBuilderException(errorMessage);
+}
