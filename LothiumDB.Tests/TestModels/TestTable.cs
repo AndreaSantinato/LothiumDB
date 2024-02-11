ff194653f420f80d7e5b0c4060011f02cfe3bb61
Andrea Santinato <santinato.andrea.santi@gmail.com>
2024-02-11 17:30:16 +0100

    Adapted the testing class model to use the new data annotations

--- LothiumDB.Tests/TestModels/TestTable.cs
+++ LothiumDB.Tests/TestModels/TestTable.cs
@@ -2,19 +2,39 @@
 
 namespace LothiumDB.Tests.TestModels;
 
-[Table("TabellaDiTest", "dbo")]
+[Table("TestTable", "dbo")]
 public class TestTable
 {
-    [Nullable]
-    [Default("")]
-    [Position(1)]
     [PrimaryKey]
-    [Column("Nome", "VARCHAR", 1)]
-    public string? PropertyName { get; set; }
+    [Position(1)]
+    [Column("PropertyName", "VARCHAR")]
+    public string Name { get; set; } = string.Empty;
     
     [Nullable]
     [Default("")]
     [Position(2)]
-    [Column("Descrizione", "VARCHAR", 2)]
-    public string? PropertyDescription { get; set; }
+    [Column("PropertyDescription", "VARCHAR")]
+    public string? Description { get; set; }
+
+    [Nullable]
+    [Default("")]
+    [Position(3)]
+    [Column("PropertyStringValue", "VARCHAR")]
+    public string? Value1 { get; set; }
+
+    [Nullable]
+    [Default(0)]
+    [Position(4)]
+    [Column("PropertyIntValue", "INT")]
+    public int? Value2 { get; set; }
+
+    [Nullable]
+    [Default("")]
+    [Position(5)]
+    [Column("PropertyDateTimeValue", "DATETIME")]
+    public DateTime? Value3 { get; set; }
+
+    [Ignore]
+    public string CombinedValues 
+        => string.Concat(Value1, Value2, Value3);
 }
